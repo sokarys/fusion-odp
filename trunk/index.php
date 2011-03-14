8e2ea4f4594fbd2def48ae632a62b072d4622c1c
Laurent.gadel@gmail.com <Laurent.gadel@gmail.com@3f43b8ee-c03c-7cc7-e424-9d7d69144f31>
2011-03-14 16:51:24 +0000

    rtst

--- trunk/index.php
+++ trunk/index.php
@@ -2,14 +2,14 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
-    <title>An XHTML 1.0 Strict standard template</title>
+    <title>Compilez vos présentations OPD</title>
     <meta http-equiv="content-type" content="text/html;charset=utf-8" />
 </head>
 <body>
     <form action="input_file.htm" enctype="multipart/form-data">
         <fieldset>
             <caption>Vos présentations à fusionner</caption>
-            <input name="fichier" type="file" size="50" maxlength="100000" accept="text/*">
+            <input name="fichier" type="file" size="50" maxlength="100000" accept="text/*"/>
         </fieldset>
     </form>
 </body>
