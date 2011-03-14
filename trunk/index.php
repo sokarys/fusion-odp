15307f820afcdd18bc454d035519d17ba44e49a7
olivier.locci <olivier.locci@3f43b8ee-c03c-7cc7-e424-9d7d69144f31>
2011-03-14 16:42:46 +0000

    la liste des taches a faire. AU BOULOT !!!!!!

---
+++ trunk/index.php
@@ -0,0 +1,15 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
+    
+<form action="input_file.htm" enctype="multipart/form-data">
+    <fieldset>
+        <caption>Vos présentation à fusionné</caption>
+        <input name="fichier" type="file" size="50" maxlength="100000" accept="text/*">
+        </fieldset>
+</form>
+    
