bcab1d2179c0ace692f2ddca86caa9597f4e9fcb
Laurent.gadel@gmail.com <Laurent.gadel@gmail.com@3f43b8ee-c03c-7cc7-e424-9d7d69144f31>
2011-03-14 16:47:45 +0000

    rtst

--- trunk/index.php
+++ trunk/index.php
@@ -1,15 +1,20 @@
-<?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 
-?>
-    
-<form action="input_file.htm" enctype="multipart/form-data">
-    <fieldset>
-        <caption>Vos présentation à fusionné</caption>
-        <input name="fichier" type="file" size="50" maxlength="100000" accept="text/*">
+<head>
+    <title>An XHTML 1.0 Strict standard template</title>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+</head>
+<body>
+    <form action="input_file.htm" enctype="multipart/form-data">
+        <fieldset>
+            <caption>Vos présentations à fusionner</caption>
+            <input name="fichier" type="file" size="50" maxlength="100000" accept="text/*">
         </fieldset>
-</form>
+    </form>
+</body>
+</html>
+
+
+
     
