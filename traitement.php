d6d35f55e86ac4c3802d8027b8faa2b6fde1c664
michael.lefevre38@gmail.com <michael.lefevre38@gmail.com@3f43b8ee-c03c-7cc7-e424-9d7d69144f31>
2011-03-18 14:16:26 +0000

    create list xml

--- traitement.php
+++ traitement.php
@@ -1,7 +1,16 @@
 <?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
+if(isset($_POST)){
+    $dom = new DomDocument('1.0', 'utf-8');
+    $node=$dom->createElement("list");
+    $dom->appendChild($node);
+    foreach($_POST['document'] as $valeur)
+    {
+        $nodeDoc=$dom->createElement("doc");
+        $nodeDoc->setAttribute("src", $valeur);
+        $node->appendChild($nodeDoc);
+    }
+    $dom->save('./xslt/list2.xml');
+}else{
+   echo("Cette page n'est pas accessible !!!");
+}
 ?>
