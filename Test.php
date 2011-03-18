93de45b5c2d21b72d4604b8bd312e43bb36c48e2
Laurent.gadel@gmail.com <Laurent.gadel@gmail.com@3f43b8ee-c03c-7cc7-e424-9d7d69144f31>
2011-03-18 14:26:07 +0000

    rtst

--- Test.php
+++
@@ -1,7 +0,0 @@
-<?php
-include('functions/unzip.php');
-include('functions/executeXSLContenu.php');
-
-unzip();
-executeXSLContenu();
-?>
--- functions/executeXSLContenu.php
+++ functions/executeXSLContenu.php
@@ -1,9 +1,10 @@
 <?php
 function executeXSLContenu(){
+
     $urlSource = "./../xslt/list.xml";
     $urlXSL = "./../xslt/copyFinal.xsl";
-    $urlTarget = "./../document/resultat/content.xml";
-    echo($urlSource);
+    $urlDirTarget = "./../document/resultat";
+    $urlTarget = $urlDirTarget."/content.xml";
     $xml = new DOMDocument;
     $xml->load($urlSource);
 
@@ -13,8 +14,10 @@ function executeXSLContenu(){
     $proc = new XSLTProcessor;
     $proc->importStyleSheet($xsl);
 
+
     $newFile = fopen($urlTarget, "w+");
     fputs($newFile, $proc->transformToXML($xml));
     fclose($newFile);
+    chmod($urlTarget,0777);
 }
 ?>
