123e4bff52b9d739224f59fd89f403394ec2e4d6
michael.lefevre38@gmail.com <michael.lefevre38@gmail.com@3f43b8ee-c03c-7cc7-e424-9d7d69144f31>
2011-03-18 13:10:21 +0000

    jquery

--- trunk/index.php
+++ trunk/index.php
@@ -6,12 +6,23 @@
     <meta http-equiv="content-type" content="text/html;charset=utf-8" />
 </head>
 <body>
-    <form action="input_file.htm" enctype="multipart/form-data">
+    <form action="index.php" enctype="multipart/form-data" method="post">
         <fieldset>
+            <caption>Modèle à appliquer à votre présentation</caption>
+            <input name="modele" type="file" size="50" maxlength="100000"/><br/>
             <caption>Vos présentations à fusionner</caption>
-            <input name="fichier" type="file" size="50" maxlength="100000" accept="text/*"/>
+            <input name="fichier[]" type="file" size="50" maxlength="100000" accept="text/*"/>
         </fieldset>
+        <input type="submit" name="Envoyer"/>
     </form>
+    <?php    
+    if(isset($_FILES['modele']['name'])){
+        echo("Votre modèle : ".$_FILES['modele']['name']);
+    }
+    else{
+       echo("Veuillez sélectionner un modèle.");
+    }
+    ?>
 </body>
 </html>
 
