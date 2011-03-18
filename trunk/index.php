5f04ac94f7d5ed39513fd177754ad62c77dd0603
michael.lefevre38@gmail.com <michael.lefevre38@gmail.com@3f43b8ee-c03c-7cc7-e424-9d7d69144f31>
2011-03-18 14:16:26 +0000

    create list xml

--- trunk/index.php
+++ trunk/index.php
@@ -4,6 +4,23 @@
 <head>
     <title>Compilez vos présentations OPD</title>
     <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+    <script src="./jquery/js/jquery-1.4.2.min.js"></script>
+    <script src="./jquery/js/jquery-ui-1.8.6.custom.min.js"></script>
+    <style>
+	#sortable { list-style-type: none; margin: 0; padding: 0; width: 60%; }
+	#sortable li { margin: 0 5px 5px 5px; padding: 5px; font-size: 1.2em; height: 1.5em; }
+	html>body #sortable li { height: 1.5em; line-height: 1.2em; }
+	.ui-state-highlight { height: 1.5em; line-height: 1.2em; }
+    </style>
+	<script>
+	$(function() {
+		$( "#sortable" ).sortable({
+			placeholder: "ui-state-highlight"
+		});
+		$( "#sortable" ).disableSelection();
+	});
+	</script>
+
 </head>
 <body>
     <form action="index.php" enctype="multipart/form-data" method="post">
@@ -23,6 +40,18 @@
        echo("Veuillez sélectionner un modèle.");
     }
     ?>
+    <form action="traitement.php" enctype="multipart/form-data" method="post">
+        <ul id="sortable">
+            <li class="ui-state-default"><input type="hidden" name=document[]" value="t1"/>Item 1</li>
+            <li class="ui-state-default"><input type="hidden" name=document[]" value="t2"/>Item 2</li>
+            <li class="ui-state-default">Item 3</li>
+            <li class="ui-state-default">Item 4</li>
+            <li class="ui-state-default">Item 5</li>
+            <li class="ui-state-default">Item 6</li>
+            <li class="ui-state-default">Item 7</li>
+        </ul>
+       <input type="submit" value="Créer la présentation"/>
+    </form>
 </body>
 </html>
 
