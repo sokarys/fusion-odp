9187994001bde563134224c5bc15a8e82625864e
Laurent.gadel@gmail.com <Laurent.gadel@gmail.com@3f43b8ee-c03c-7cc7-e424-9d7d69144f31>
2011-03-30 09:27:17 +0000

    xptdr2

--- trunk/scripts/Test.php
+++ trunk/scripts/Test.php
@@ -1,8 +1,14 @@
 <?php
 include('./../functions/chmodr.php');
 include('./../functions/unzip.php');
+include('./../functions/zip.php');
 include('./../functions/executeXSLContenu.php');
+include('./../functions/recursiveCopy.php');
+include('./../functions/picturesCopy.php');
 
 //unzip();
-executeXSLContenu();
+//smartCopy('./../xslt/tmp/model', './../document/resultat');
+//picturesCopy();
+//executeXSLContenu();
+zip();
 ?>
