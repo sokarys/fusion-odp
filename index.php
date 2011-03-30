4339f08e29dad1fcbf7b737647d2121bd0b11c07
michael.lefevre38@gmail.com <michael.lefevre38@gmail.com@3f43b8ee-c03c-7cc7-e424-9d7d69144f31>
2011-03-30 08:55:46 +0000

    upload

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 <head>
     <title>Compilez vos présentations OPD</title>
     <meta http-equiv="content-type" content="text/html;charset=utf-8" />
-    <link rel="stylesheet" href="./jquery/jquery-upload/jquery.fileupload-ui.css"/>
+    <link rel="stylesheet" href="./jquery/css/ui-lightness/jquery-ui-1.8.6.custom.css"/>
+    <link rel="stylesheet" href="style.css"/>
     <script src="./jquery/js/jquery-1.4.2.min.js"></script>
     <script src="./jquery/js/jquery-ui-1.8.6.custom.min.js"></script>
     
@@ -24,34 +25,6 @@
 
 </head>
 <body>
-    <form id="file_upload" action="index.php" method="POST" enctype="multipart/form-data">
-    <input type="file" name="file" multiple/>
-    <button>Upload</button>
-    <div>Upload files</div>
-    </form>
-    <table id="files"></table>
-        <script src="./jquery/jquery-upload/jquery.fileupload.js"></script>
-        <script src="./jquery/jquery-upload/jquery.fileupload-ui.js"></script>
-        <script>
-        /*global $ */
-        $(function () {
-            $('#file_upload').fileUploadUI({
-                uploadTable: $('#files'),
-                downloadTable: $('#files'),
-                buildUploadRow: function (files, index) {
-                    return $('<tr><td>' + files[index].name + '<\/td>' +
-                            '<td class="file_upload_progress"><div><\/div><\/td>' +
-                            '<td class="file_upload_cancel">' +
-                            '<button class="ui-state-default ui-corner-all" title="Cancel">' +
-                            '<span class="ui-icon ui-icon-cancel">Cancel<\/span>' +
-                            '<\/button><\/td><\/tr>');
-                },
-                buildDownloadRow: function (file) {
-                    return $('<tr><td>' + file.name + '<\/td><\/tr>');
-                }
-            });
-        });
-        </script>
     <form action="index.php" enctype="multipart/form-data" method="post">
         <fieldset>
             <caption>Modèle à appliquer à votre présentation</caption>
@@ -63,41 +36,42 @@
     </form>
     <?php
     //upload
-    $dossier = './document/temp/';
-    print_r($_FILES);
-    $fichier = basename($_FILES['fichier']['name'][0]);
-    $extensions = array('.odp');
-    $extension = strrchr($_FILES['fichier']['name'][0], '.');
-    //Début des vérifications de sécurité...
-    if(!in_array($extension, $extensions)) //Si l'extension n'est pas dans le tableau
-    {
-         $erreur = 'Vous devez uploader un fichier de type odp.';
-    }
-    if(!isset($erreur)) //S'il n'y a pas d'erreur, on upload
-    {
-         //On formate le nom du fichier ici...
-         $fichier = strtr($fichier,
-              'ÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìíîïðòóôõöùúûüýÿ',
-              'AAAAAACEEEEIIIIOOOOOUUUUYaaaaaaceeeeiiiioooooouuuuyy');
-         $fichier = preg_replace('/([^.a-z0-9]+)/i', '-', $fichier);
-         if(move_uploaded_file($_FILES['fichier']['tmp_name'][0], $dossier . $fichier)) //Si la fonction renvoie TRUE, c'est que ça a fonctionné...
-         {
-              echo 'Upload effectué avec succès !';
-         }
-         else //Sinon (la fonction renvoie FALSE).
-         {
-              echo 'Echec de l\'upload !';
-         }
-    }
-    else
-    {
-         echo $erreur;
-    }
-    if(isset($_FILES['modele']['name'])){
-        echo("Votre modèle : ".$_FILES['modele']['name']);
-    }
-    else{
-       echo("Veuillez sélectionner un modèle.");
+    if(isset($_FILES['fichier'])){
+        $dossier = './document/temp/';
+        $fichier = basename($_FILES['fichier']['name'][0]);
+        $extensions = array('.odp');
+        $extension = strrchr($_FILES['fichier']['name'][0], '.');
+        //Début des vérifications de sécurité...
+        if(!in_array($extension, $extensions)) //Si l'extension n'est pas dans le tableau
+        {
+             $erreur = 'Vous devez uploader un fichier de type odp.';
+        }
+        if(!isset($erreur)) //S'il n'y a pas d'erreur, on upload
+        {
+             //On formate le nom du fichier ici...
+             $fichier = strtr($fichier,
+                  'ÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìíîïðòóôõöùúûüýÿ',
+                  'AAAAAACEEEEIIIIOOOOOUUUUYaaaaaaceeeeiiiioooooouuuuyy');
+             $fichier = preg_replace('/([^.a-z0-9]+)/i', '-', $fichier);
+             if(move_uploaded_file($_FILES['fichier']['tmp_name'][0], $dossier . $fichier)) //Si la fonction renvoie TRUE, c'est que ça a fonctionné...
+             {
+                  echo 'Upload effectué avec succès !';
+             }
+             else //Sinon (la fonction renvoie FALSE).
+             {
+                  echo 'Echec de l\'upload !';
+             }
+        }
+        else
+        {
+             echo $erreur;
+        }
+        if(isset($_FILES['modele']['name'])){
+            echo("Votre modèle : ".$_FILES['modele']['name']);
+        }
+        else{
+           echo("Veuillez sélectionner un modèle.");
+        }
     }
     ?>
 
@@ -105,23 +79,22 @@
 
 
     <form action="traitement.php" enctype="multipart/form-data" method="post">
-        <ul id="sortable">
-        <?php
-             $dirname = './document/temp';
-             $dir = opendir($dirname);
-             while($file = readdir($dir)) {
-                if($file != ".svn" && $file != '.' && $file != '..' && !is_dir($dirname.$file))
-                {
-		echo '<li class="ui-state-default"><input type="hidden" name="document[]" value="'.$file.'"/>'.$file.'</li>';
-                }
-                }
-        ?>
+        
+        <div class="block_fichier ui-widget-header">
+            <ul id="sortable">
+            <?php
+                 $dirname = './document/temp';
+                 $dir = opendir($dirname);
+                 while($file = readdir($dir)) {
+                    if($file != ".svn" && $file != '.' && $file != '..' && !is_dir($dirname.$file))
+                    {
+                    echo '<li class="ui-state-default"><input type="hidden" name="document[]" value="'.$file.'"/>'.$file.'</li>';
+                    }
+                    }
+            ?>
         </ul>
+        </div>
        <input type="submit" value="Créer la présentation"/>
     </form>
 </body>
 </html>
-
-
-
-    
--- jquery/jquery-upload/example/upload.php
+++ jquery/jquery-upload/example/upload.php
@@ -1,4 +1,36 @@
 <?php
 $file = $_FILES['file'];
-echo '{"name":"'.$file['name'].'","type":"'.$file['type'].'","size":"'.$file['size'].'"}';
+echo 'plop';
+        //upload
+        $dossier = './document/temp/';
+        $fichier = basename($file['name']);
+        $extensions = array('.odp');
+        $extension = strrchr($file['name'], '.');
+        //Début des vérifications de sécurité...
+        if(!in_array($extension, $extensions)) //Si l'extension n'est pas dans le tableau
+        {
+             $erreur = 'Vous devez uploader un fichier de type odp.';
+        }
+        if(!isset($erreur)) //S'il n'y a pas d'erreur, on upload
+        {
+             //On formate le nom du fichier ici...
+             $fichier = strtr($fichier,
+                  'ÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìíîïðòóôõöùúûüýÿ',
+                  'AAAAAACEEEEIIIIOOOOOUUUUYaaaaaaceeeeiiiioooooouuuuyy');
+             $fichier = preg_replace('/([^.a-z0-9]+)/i', '-', $fichier);
+             if(move_uploaded_file($file['tmp_name'], $dossier . $fichier)) //Si la fonction renvoie TRUE, c'est que ça a fonctionné...
+             {
+                  //echo 'Upload effectué avec succès !';
+             }
+             else //Sinon (la fonction renvoie FALSE).
+             {
+                 // echo 'Echec de l\'upload !';
+             }
+        }
+        if(isset($_FILES['modele']['name'])){
+            echo("Votre modèle : ".$_FILES['modele']['name']);
+        }else{
+           echo("Veuillez sélectionner un modèle.");
+        }
+
 ?>
