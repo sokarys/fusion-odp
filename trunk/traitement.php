5405305010f234a3a37e9d83867aae4c06a5663a
michael.lefevre38@gmail.com <michael.lefevre38@gmail.com@3f43b8ee-c03c-7cc7-e424-9d7d69144f31>
2011-03-30 09:30:20 +0000

    traitement

--- trunk/traitement.php
+++ trunk/traitement.php
@@ -1,6 +1,11 @@
 <?php
-include_once 'functions/unzip.php';
-include_once 'functions/executeXSLContenu.php';
+include('./../functions/chmodr.php');
+include('./../functions/unzip.php');
+include('./../functions/zip.php');
+include('./../functions/executeXSLContenu.php');
+include('./../functions/recursiveCopy.php');
+include('./../functions/picturesCopy.php');
+
 if(isset($_POST)){
     $dom = new DomDocument('1.0', 'utf-8');
     $node=$dom->createElement("list");
@@ -14,6 +19,8 @@ if(isset($_POST)){
     $dom->save('./xslt/list.xml');
 
     unzip();
+    smartCopy('./../xslt/tmp/model', './../document/resultat');
+    picturesCopy();
     executeXSLContenu();
 }else{
    echo("Cette page n'est pas accessible !!!");
