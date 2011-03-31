2f07c63b854bbf9af3779ddd5bb5f6f0bc310427
olivier.locci <olivier.locci@3f43b8ee-c03c-7cc7-e424-9d7d69144f31>
2011-03-31 13:51:43 +0000

    sdf

--- trunk/functions/executeXSLContenu.php
+++ trunk/functions/executeXSLContenu.php
@@ -6,20 +6,21 @@ function executeXSLContenu(){
 
     $urlSource = "./../xslt/list.xml";
     $urlXSL = "./../xslt/copyFinal.xsl";
-    $urlDirTarget = "./../document/resultat";
-    $urlTarget = $urlDirTarget."/content.xml";
-    $xml = new DOMDocument;
+    $urlDirTarget = "./../document/resultat/";
+    $urlTarget = $urlDirTarget."content.xml";
+    $xml = new DOMDocument();
     $xml->load($urlSource);
 
-    $xsl = new DOMDocument;
+    $xsl = new DOMDocument();
     $xsl->load($urlXSL);
 
-    $proc = new XSLTProcessor;
+    $proc = new XSLTProcessor();
     $proc->importStyleSheet($xsl);
 
 
     $newFile = fopen($urlTarget, "w+");
-    fputs($newFile, $proc->transformToXML($xml));
+    fputs($newFile, $proc->transformToXml($xml));
+    
     fclose($newFile);
     chmod($urlTarget,0777);
 }
