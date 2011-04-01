d66773b8c7ce2654beb7b85ac17411e9e0d6a6bb
Laurent.gadel@gmail.com <Laurent.gadel@gmail.com@3f43b8ee-c03c-7cc7-e424-9d7d69144f31>
2011-04-01 12:21:25 +0000

    xptdr2...

---
+++ functions/deleteODPFIle.php
@@ -0,0 +1,20 @@
+<?php
+
+function deleteODPFile($fileName,$model){
+    echo "***********************************************<br/>";
+    echo " DELETE ODP FILE<br/>";
+    echo "***********************************************<br/>";
+
+    if(isset($model) && $model == true){
+        $ODPTempDirectory = "./../document/temp_model/";
+    }else{
+        $ODPTempDirectory = "./../document/temp/";
+    }
+
+    if(is_file($ODPTempDirectory.$fileName)){
+        if(unlink($ODPTempDirectory.$fileName)){ echo $ODPTempDirectory.$fileName."<br/>"; }
+    }else{
+        echo "ERROR - File not found: ".$ODPTempDirectory.$fileName."<br/>";
+    }
+}
+?>
