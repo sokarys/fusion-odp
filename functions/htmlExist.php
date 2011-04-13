4c1fab48a50eb5994631988a6641267efa242e73
michael.lefevre38@gmail.com <michael.lefevre38@gmail.com@3f43b8ee-c03c-7cc7-e424-9d7d69144f31>
2011-04-13 19:16:00 +0000

    php

--- functions/htmlExist.php
+++ functions/htmlExist.php
@@ -3,9 +3,9 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-function htmlExiste($name){
+function htmlExist($name){
 
-     $dirname = './../document/temp_model/';
+     $dirname = './document/final/';
          $dir = opendir($dirname);
          while($file = readdir($dir)) {
             if(strstr($file, $name.'.hmtl') && !is_dir($dirname.$file))
