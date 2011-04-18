e66962da9811fdb14693249bee7df99ce165d466
michael.lefevre38@gmail.com <michael.lefevre38@gmail.com@3f43b8ee-c03c-7cc7-e424-9d7d69144f31>
2011-04-18 10:43:37 +0000

    php

--- trunk/functions/deleteODP.php
+++ trunk/functions/deleteODP.php
@@ -1,11 +1,16 @@
 <?php
 include 'deleteODPFIle.php';
+include 'deleteResultat.php';
 if(isset($_GET['model'])){
     deleteODPFile($_GET['model'],true);
 
 }
 if(isset($_GET['fichier'])){
     deleteODPFile($_GET['fichier'],false);
+
+}
+if(isset($_GET['final'])){
+    deleteODPFile($_GET['final'],false);
 
 }
 ?>
