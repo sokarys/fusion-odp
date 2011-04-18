354c0bd02750310a14ba7636f6d84b80b90eb5cb
michael.lefevre38@gmail.com <michael.lefevre38@gmail.com@3f43b8ee-c03c-7cc7-e424-9d7d69144f31>
2011-04-18 10:40:51 +0000

    php

--- odp.php
+++ odp.php
@@ -4,15 +4,29 @@
 <link rel="stylesheet" type="text/css" href="./timesheets/demo/style/transitions.css">
 <link rel="stylesheet" type="text/css" href="./timesheets/demo/style/slideshow.css">
 <script type="text/javascript" src="./timesheets/timesheets.js"></script><script type="text/javascript" src="./timesheets/timesheets-navigation.js"></script>
+<script>
+            function deleteFinal(fichier){
+            $.ajax({
+                type:"GET",
+                url:"./functions/deleteODP.php?final="+fichier,
+                success: function(msg){
+                    document.location.href="ODP.php";
+                }
+            })
+        }
+</script>
 <style type="" >
 	#sortable { list-style-type: none; margin: 0; padding: 0;}
 	#sortable li { margin: 0 5px 5px 5px; padding: 5px; font-size: 1.2em; height: 1.5em; }
 	html>body #sortable li { height: 1.5em; line-height: 1.2em; }
 	.ui-state-highlight { height: 1.5em; line-height: 1.2em; }
 </style>
+
 <?php
+include('./functions/htmlExist.php');
+include('./functions/deleteResultat.php');
 if(isset($_GET['resultat'])){
-    include('./functions/htmlExist.php');
+    
     require_once('./document/final/'.$_GET['resultat'].'.html');
 }
 ?>
@@ -35,15 +49,19 @@ if(isset($_GET['resultat'])){
         if($file != ".svn" && $file != '.' && $file != '..' && !is_dir($dirname.$file) && strstr($file, '.html')==false)
         {
            if($courant<$count){
-               echo '<li class="ui-state-default"><img src="./jquery/css/icon_delete_action.gif" onclick="javascript:delet(\''.$file.'\')"/>';
-               if(isset($_GET['resultat']) && htmlExist($_GET['resultat'])){
-                  echo '<a href="odp.php?resultat='.strstr($file, '.', true).'"><img src="./jquery/css/HTML5.png"/></a>';
+               echo '<li class="ui-state-default"><img src="./jquery/css/icon_delete_action.gif" onclick="deleteResultat(\''.$file.'\')"/>';
+               if(htmlExist(strstr($file, '.', true))){
+                  echo '<a href="odp.php?resultat='.strstr($file, '.', true).'"><img src="./jquery/css/HTML5.png" style="border:none;width:32px;padding:2px;"/></a>';
                }           
                echo '<a href="'.$dirname.'/'.$file.'">'.$file.'</a><input type="hidden" name="modele[]" value="'.$file.$courant.'"/></li>';
                $courant++;
 
            }else{
-               echo '<li class="ui-state-default"><img src="./jquery/css/icon_delete_action.gif" onclick="javascript:delet(\''.$file.'\')"/><a href="'.$dirname.'/'.$file.'">'.$file.'</a><input type="hidden" name="modele[]" value="'.$file.'"/><img src="./jquery/css/new.png" style="padding-left:50%;"></li>';
+               echo '<li class="ui-state-default"><img src="./jquery/css/icon_delete_action.gif" onclick="deleteResultat(\''.$file.'\')"/>';
+               if(htmlExist(strstr($file, '.', true))){
+                  echo '<a href="odp.php?resultat='.strstr($file, '.', true).'"><img src="./jquery/css/HTML5.png" style="border:none;width:32px;padding:2px;"/></a>';
+               }
+                echo '<a href="'.$dirname.'/'.$file.'">'.$file.'</a><input type="hidden" name="modele[]" value="'.$file.'"/><img src="./jquery/css/new.png" style="padding-left:50%;"></li>';
            }
         }
      }
